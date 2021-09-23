6e82cee213a0117b97dfe02c95ed8130dbc6f039
David_Gavilanes <davidgavilanesvelez@gmail.com>
2021-09-23 22:16:10 +0200

    adde docker and post model

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class User extends Model
+{
+  use Sluggable;
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'title',
+    'body',
+    'type',
+    'slug'
+  ];
+
+  public function sluggable(): array
+  {
+    return [
+      'slug' => [
+        'source' => 'title'
+      ]
+    ];
+  }
+}
